4cabcbfb22294c4315a7270ad2575459995c8568
lars <ll.copenhagen@gmail.com>
2020-12-20 13:48:50 +0100

    app text change

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          hjShettio Editttt <code>src/App.js</code> and save to reload.
+          yaycheckouthjShettio Editttt <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
